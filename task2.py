c8a9a68aabc4d4e7899a322881878ee582443351
gina khayatunnufus <khayatunnufus@gmail.com>
2016-02-18 05:49:41 -0800

    Upload file task1, task2, task3

---
+++ task2.py
@@ -0,0 +1,10 @@
+def calculate_triangle(height, base):
+    return height * base / 2
+
+
+result1 = calculate_triangle(10, 2)
+result2 = calculate_triangle(30, 5)
+result3 = calculate_triangle(15, 2)
+print("Result 1 = ", result1)
+print("Result 2 = ", result2)
+print("Result 3 = ", result3)
